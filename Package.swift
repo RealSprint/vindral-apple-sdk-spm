c80109d7590e109ed2d02c4c7702abced64a8ad5
Erik Jakobsson <erik.jakobsson@realsprint.com>
2023-02-16 16:23:23 +0100

    Update latest version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VindralStreaming",
-            url: "https://applesdk.vindral.com/tag/1.0.0/VindralStreaming.zip",
-            checksum: "6084bf12811c5e9a4d4f4d4203af76d9490f3d8a7f486808837571cbf5a541c3"),
+            url: "https://applesdk.vindral.com/tag/1.0.1/VindralStreaming.zip",
+            checksum: "e00bdf078a57099248d63914b8f10f3ae94186634ff6f79a1c2131fb8e3dfca4"),
     ]
 )
